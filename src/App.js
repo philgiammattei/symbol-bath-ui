630c4ce42b78b20c3515683f3a2ceb920fbfb389
Phil Giammattei <philgiammattei@gmail.com>
2022-08-09 13:11:03 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>symbol bath</h2>
+        <h4>divination tools for digital scryers</h4>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://astrodice.netlify.app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          astrodice
+        </a>
+        <a
+          className="App-link"
+          href="https://book-of-changes.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          book of changes
+        </a>
+        <a
+          className="App-link"
+          href="https://twitter.com/phil__harmonic"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p><svg class="twitter-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" /></svg></p>
         </a>
       </header>
     </div>
